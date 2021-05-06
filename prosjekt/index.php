6da3dfbe5aedd9e45edf89f7b8e4b44aa798feb2
Eiriksol <eirsol17@asvg.no>
2021-05-06 09:39:16 +0200

    Opprettet kobling
    Vellykket kobling fra dokument til database

--- prosjekt/index.php
+++ prosjekt/index.php
@@ -10,10 +10,27 @@
     
 <?php
 
+//Tilkoblingsinformasjon:
+$tjener = "localhost";
+$brukernavn = "root";
+$passord = "";
+$database = "mydb";
+
+//Opprette en kobling:
+$kobling = new mysqli($tjener, $brukernavn, $passord, $database);
+
+//Angi UTF-8 som tegnesett:
 $kobling->set_charset("utf8");
 
+//Sjekker om tilkobling virker:
+if ($kobling->connect_error) {
+    die("Noe gikk galt: " . $kobling->connect_error);
+}
+
+$sql = "SELECT * FROM produkt";
+$resultat = $kobling->query($sql);
+echo "Spørringen ga $resultat->num_rows rader.";
+
 ?>
-
-
 </body>
 </html>
